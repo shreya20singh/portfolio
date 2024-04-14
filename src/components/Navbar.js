3c206764eb954514c80b1bd895361d3033de9cbe
Shreya Singh <shreyasingh200297.ss@gmail.com>
2024-04-14 16:40:28 -0400

    removing resume section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -77,7 +77,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 as={Link}
                 to="/resume"
@@ -85,7 +85,7 @@ function NavBar() {
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
             
           </Nav>
         </Navbar.Collapse>
