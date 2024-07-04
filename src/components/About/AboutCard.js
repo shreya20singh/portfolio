8897560f2618ecd5040145a3688c8e71c78e255c
Shreya Singh <shreyasingh200297.ss@gmail.com>
2024-07-04 13:49:41 -0700

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Shreya Singh </span>
-            living in <span className="purple"> Jersey City, NJ</span>
+            living in <span className="purple"> Sunnyvale, CA</span>
             <br />
             I am currently a freelancer looking for <span className="purple"> full time roles </span> and freelancing gigs.
             <br />
